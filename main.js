dae3f3a0903e5ffde9612e23b4e836977e6bba55
shivansh payak <75551006+151616@users.noreply.github.com>
2022-02-13 02:20:13 -0500

    Add files via upload

---
+++ main.js
@@ -0,0 +1,22 @@
+function openNav() {
+    document.getElementById("mySidebar").style.width = "250px";
+  }
+  
+  function closeNav() {
+    document.getElementById("mySidebar").style.width = "0";
+  }
+  function bengali_page(){
+      window.location = "bengali-songs.html";
+  }
+  function marathi_page(){
+      window.location = "marathi-songs.html";
+  }
+  function tamil_page(){
+      window.location = "tamil-songs.html";
+  }
+  function telegu_page(){
+      window.location = "telegu-songs.html";
+  }
+function hindi_page(){
+    window.location = "hindi-songs.html";
+}
